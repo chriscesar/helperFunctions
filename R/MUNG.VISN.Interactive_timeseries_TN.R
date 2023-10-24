746b394e4e59a6d7e452d0e636d233a5c9c15498
Christopher Cesar <3082834+chriscesar@users.noreply.github.com>
2023-10-24 08:00:16 +0100

    Update MUNG.VISN.Interactive_timeseries_TN.R
    remove explicit filepaths

--- R/MUNG.VISN.Interactive_timeseries_TN.R
+++ R/MUNG.VISN.Interactive_timeseries_TN.R
@@ -1,4 +1,3 @@
-setwd("G:/N_Marine/04 A & R/Technical Guidance/Statistics Group ECMAS d/R code")
 
 library(dygraphs)
 library(tidyverse)
@@ -9,20 +8,15 @@ library(astsa)
 
 
 #load the wims_d dataset 
-
-wims.d <- read.csv("G:\\N_Marine\\07 Training & Reference Documents\\A&R Technical Guidance\\Statistics Group ECMAS d\\R code\\datasets\\wims_d.csv")
+wims.d <- read.csv("path/to/data.csv")
 
 
 #Subset by variables of interest - here DIN & Chlorophyll
-
 DIN <- wims.d[which(wims.d$Det.Name %in% c('Nitrogen, Dissolved Inorganic : as N')), ]
 DIN <- rename(DIN, DIN = Res)
 
-
 Chla <- wims.d[which(wims.d$Det.Name %in% c('Chlorophyll : Acetone Extract')), ]
 Chla <- rename(Chla, Chla = Res)
-
-
 
 df <- merge(Chla, DIN, by= c("PtCode", "Date"), all.y=T, all.x=T)
 
@@ -36,15 +30,11 @@ df$Date <- as.Date(df$Date, origin="1899-12-30") # convert from numeric to dates
 plot(df$Date, df$DIN)
 
 #hmmm it looks like there's a major outlier in 2013. Let's remove it - well let's just remove any DIN values >500
-
 df <- df [which(df$DIN < 500),]
 plot(df$Date, df$DIN)# that's better!
 
 # plot/check for outliers
-
 plot(df$Date, df$Chla) # looks ok?
-
-
 
 #coerce to xts timeseries format 
 df_d <- df$Date
@@ -52,11 +42,9 @@ vars <- c("Chla", "DIN")
 df_ts <- df[vars]
 ts1 <- xts(df_ts, order.by = df_d)
 
-
 rm(DIN, Chla, wims.d) #cleanup
 
 # Plot as interactive timeseries graph 
-
 dyHide <-function(dygraph) {
   dyPlugin(
     dygraph = dygraph,
@@ -86,28 +74,24 @@ ts_Chla <- df %>% group_by(Date) %>% summarise(
   )
 
 # where there was only one observation for a date then the SD summarisation produces NaNs - let's replace these with 0
-
 ts_Chla$Chla_sd[is.nan(ts_Chla$Chla_sd)] <- 0 
 
 #and now use these to add upper and lower values to the means 
-
 ts_Chla$Chla_uv <- (ts_Chla$Chla + ts_Chla$Chla_sd)
 ts_Chla$Chla_lv <- (ts_Chla$Chla - ts_Chla$Chla_sd)
 ts_Chla$Chla_lv[ts_Chla$Chla_lv < 0] <- 0
 ts_Chla$Chla_uv [is.na (ts_Chla$Chla_uv)] <-0
 Chla_d <- ts_Chla$Date
-#now remove the variables Chla_sd and DIN_sd 
 
+#now remove the variables Chla_sd and DIN_sd 
 vars<- c("Chla", "Chla_uv", "Chla_lv")
 
 ts_Chla <- ts_Chla[vars]
 
 #and coerce to xts object 
-
 ts_Chla <- xts(ts_Chla, order.by = Chla_d)
 
 #plot 
-
 p2 <- dygraph(ts_Chla, xlab="Date", group= "DinCha") %>% 
   dyAxis("y", label = "Chla (ug/L)", valueRange = c(0, 200), logscale = T)%>%
   dySeries(c("Chla_lv", "Chla", "Chla_uv"), label="Chla", axis=('y'), color="green") %>%
@@ -115,7 +99,6 @@ p2 <- dygraph(ts_Chla, xlab="Date", group= "DinCha") %>%
 p2
 
 # Now for DIN
-
 ts_DIN <- df %>% group_by(Date) %>% summarise(
   DIN_sd = sd(DIN),
   DIN = mean(DIN) 
@@ -139,16 +122,13 @@ p3 <- dygraph(ts_DIN, xlab="Date", group= "DinCha") %>%
 p3
 
 #sychronise & plot together - now try zooming in to any series...
-
 browsable(
   tagList(
    p1,p2,p3
   )
 )
 
-
 # Explore seasonal trends by decomposing the timeseries using Loess
-
 DIN.ts <- ts(df$DIN, start = c(2007,1), end = c(2018,3), frequency = 365)
 DIN_decomposed <- stl(DIN.ts, "periodic")
 plot(DIN_decomposed)
@@ -162,11 +142,8 @@ Chla.ts <- ts(Chla.ts,start = c(2007,1), end = c(2018,3), frequency = 365)
 Chla_decomposed <- stl(Chla.ts, "periodic")
 plot(Chla_decomposed)
 
-#maybe we just went to extract a component of the decompostion i.e. to compare the trends
-
-#this can be done like this: 
-
-
+# maybe we just went to extract a component of the decompostion i.e. to compare the trends
+# this can be done like this: 
 
 Chla_trend <- stl(Chla.ts, s.window="periodic")$time.series[,"trend"]
 
@@ -200,9 +177,7 @@ cf
 max(cf$acf)
 
 #so the covariance is greatest (0.120645) where the lag is -6 - but similar correlations exist for lags of +5 and +6 too (for example)
-
 #another way to view this is by making scatterplots of the lagged relationhip between DIN (lagged series) and Chla
-
 lag2.plot(DIN, Chla, max.lag = 20, corr = T) # In each plot, observed Chla is on the vertical and a past lag of DIN is on the horizontal.  Correlation values are given on each plot.
 
 alldata <- ts.intersect(rec,reclag1=lag(rec,-1), reclag2=lag(rec,-2), soilag5 = lag(soi,-5),
